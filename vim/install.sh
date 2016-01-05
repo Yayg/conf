c466603a85d13717a83b5ae26645f88d49f9ca23
yayg <rafael.gozlan@epita.fr>
2016-01-05 18:43:50 +0100

    end

--- vim/install.sh
+++ vim/install.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.vim/colors ~/.vim/bundle
 curl https://raw.githubusercontent.com/Yayg/conf/master/vim/colors/railscasts.vim > ~/.vim/colors/railscasts.vim
 curl https://raw.githubusercontent.com/Yayg/conf/master/vim/ycm_extra_conf.py > ~/.ycm_extra_conf.py
 curl https://raw.githubusercontent.com/Yayg/conf/master/vim/vimrc > ~/.vim/vimrc
+curl https://raw.githubusercontent.com/Yayg/conf/master/vim/buffer.vim > ~/.vim/buffer.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 . ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
